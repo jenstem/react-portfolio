b1d3b0a8363255507c60cedd01ea3df429aef3eb
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-31 14:59:09 -0400

    Add index.js to Header folder

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,1 +1,17 @@
+import React from "react";
+import Navigation from "../Navigation/Navbar";
 
+export default function Header(props) {
+
+  return (
+    <div>
+      <header>
+        <Navigation />
+
+      </header>
+      <main>
+
+      </main>
+    </div >
+  );
+}
