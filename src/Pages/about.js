c90cceb1755c9e39d5021677c10cf96426edfd1e
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-31 15:14:03 -0400

    Add about.js to Pages folder

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import './about.css';
 
+
+export default function About() {
+  return (
+    <div className="about-container">
+      <h2 className="about-title">About Me</h2>
+      <p className="about-text">
+        I'm currently enrolled in the Full Stack Coding Bootcamp at the University of Connecticut.  I aim to create web applications that are both functional and aesthetically pleasing.  I have experience with HTML, CSS, JavaScript, Node.js, Express.js, MySQL, MongoDB, and React.js.  I am currently learning the MERN stack.
+
+      </p>
+      <div className="bar"></div>
+    </div>
+  );
+}
