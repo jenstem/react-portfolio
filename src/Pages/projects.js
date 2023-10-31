993634f40d38502073db6a5e69e67f964b1ba74d
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-31 18:31:36 -0400

    Revise projects.js import PWA img

--- src/Pages/projects.js
+++ src/Pages/projects.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ProjectCard from '../components/Project/ProjectCard';
 import './projects.css';
+import PWA from '../assets/app-images/PWA.jpg';
 
 const projectData = [
-    { title: 'PWA Text Editor', description: 'Text editor web application', imageUrl: "https://github.com/jenstem/react-portfolio/blob/main/src/assets/app-images/PWA.jpg", url: "https://github.com/jenstem/pwa-text-editor", site: "https://hidden-ocean-30849-b1dc3b32b941.herokuapp.com/" },
+    { title: 'PWA Text Editor', description: 'Text editor web application', imageUrl: { PWA }, url: "https://github.com/jenstem/pwa-text-editor", site: "https://hidden-ocean-30849-b1dc3b32b941.herokuapp.com/" },
     { title: 'Social Network API', description: 'A command line invoked network api', imageUrl: "../assets/app-images/social-network.jpg", url: 'https://github.com/jenstem/social-network-api', site: "Not Available" },
     { title: 'Tech Blog', description: 'CMS-style blog site', imageUrl: "./assets/app-images/tech-blog", url: "https://github.com/jenstem/tech-blog", site: "https://fast-brook-86367-d21f795e7f85.herokuapp.com/" },
     { title: 'Rhythm Reviews', description: 'A CMS-style review site', imageUrl: "../../assets/app-images/rhythm-reviews.jpg", url: "https://github.com/group7-project2/rhythm-reviews", site: "https://rhythm-reviews-8a43a75413f9.herokuapp.com/" },
