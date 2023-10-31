68604be7763b068379538b1bd928eabf47893ea4
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-31 15:18:34 -0400

    Add App.js to src folder

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Home from './Pages/home';
+import About from './Pages/about';
+import Header from './components/Header/index';
+import Footer from './components/Footer';
+import Projects from './Pages/projects';
+import Contact from './Pages/contact';
+import Resume from './Pages/resume';
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <div id='home'>
+                <Home />
+            </div>
+            <div id='about'>
+                <About />
+            </div>
+            <Projects />
+            <Resume />
+            <Contact />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
