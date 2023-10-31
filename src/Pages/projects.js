bb7eec55d9e7af32d7ca465e92243529cd14f683
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-31 15:16:09 -0400

    Add projects.js to Pages folder

---
+++ src/Pages/projects.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ProjectCard from '../components/Project/ProjectCard';
+import './projects.css';
+
+const projectData = [
+    { title: 'PWA Text Editor', description: 'Text editor web application', imageUrl: '', url: "https://github.com/jenstem/pwa-text-editor", site: "https://hidden-ocean-30849-b1dc3b32b941.herokuapp.com/" },
+    { title: 'Social Network API', description: 'A command line invoked network api', imageUrl: '', url: 'https://github.com/jenstem/social-network-api', site: "Not Available" },
+    { title: 'Tech Blog', description: 'CMS-style blog site', imageUrl: '', url: "https://github.com/jenstem/tech-blog", site: "https://fast-brook-86367-d21f795e7f85.herokuapp.com/" },
+    { title: 'Rhythm Reviews', description: 'A CMS-style review site', imageUrl: '', url: "https://github.com/group7-project2/rhythm-reviews", site: "https://rhythm-reviews-8a43a75413f9.herokuapp.com/" },
+    { title: 'E-Commerce Backend', description: 'Express.js API', imageUrl: '', url: "https://github.com/jenstem/e-commerce-backend", site: "Not Available" },
+    { title: 'Employee Tracker', description: 'Command line application', imageUrl: '', url: "https://github.com/jenstem/employee-tracker", site: "Not Available" },
+];
+
+export default function Projects() {
+    return (
+        <div id="projects" className="projects-container">
+            <h2 className="portfolio-size">My Portfolio</h2>
+            <div className="projects-grid">
+                {projectData.map((project, index) => (
+                    <a
+                        key={index}
+                    >
+                        <ProjectCard {...project} />
+                    </a>
+                ))}
+            </div>
+            <div className="add-space"></div>
+            <div className="bar"></div>
+        </div>
+    );
+};
