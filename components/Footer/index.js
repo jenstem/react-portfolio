7f0290729adce6cb21e9929dfd80303ede99b24a
jenstem <124745888+jenstem@users.noreply.github.com>
2023-10-31 14:57:57 -0400

    Add index.js to Footer folder

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,1 +1,23 @@
+import React from "react";
+import gitIcon from '../../assets/gitHubIcon.png';
+import linkedInIcon from '../../assets/linkedInIcon.png';
+import stackOverFlowIcon from '../../assets/stackoverflowIcon.png';
+import './footer.css';
 
+export default function Footer() {
+    return (
+        <footer id="footer">
+            <div>
+                <a href="https://github.com/jenstem?tab=repositories">
+                    <img src={gitIcon} alt="GitHub" className="icon"></img>
+                </a>
+                <a href="https://www.linkedin.com/in/jennifer-s-913445265/">
+                    <img src={linkedInIcon} alt="LinkedIn" className="icon"></img>
+                </a>
+                <a href="https://stackoverflow.com/users/22820388/astranova">
+                    <img src={stackOverFlowIcon} alt="StackOverflow" className="icon2"></img>
+                </a>
+            </div>
+        </footer>
+    );
+}
