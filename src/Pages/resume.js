a0d8e637583c033fbe65572f1b35c0f846518f29
jenstem <124745888+jenstem@users.noreply.github.com>
2023-11-01 11:52:58 -0400

    Revise resume.js rephrase link to resume

--- src/Pages/resume.js
+++ src/Pages/resume.js
@@ -8,11 +8,10 @@ export default function Resume() {
         <div>
             <h2 id="resume" className="resume-title">Resume & Skill Set</h2>
             <p>
-                Click {" "}
+                {" "}
                 <span>
-                    <a href={resumeDoc} target="_blank" rel="noreferrer" className='emoji'>📄</a>
+                    <a href={resumeDoc} target="_blank" rel="noreferrer" className='download-resume'>Click here to download my resume PDF</a>
                 </span>{" "}
-                to download a pdf of my resume.
             </p>
                 <p>HTML &#x2022; CSS &#x2022; JavaScript &#x2022; jQuery &#x2022; React &#x2022; Tailwind &#x2022; Bootstrap &#x2022; Adobe Photoshop</p>
                 <p className='add-space'>MySQL &#x2022; MongoDB &#x2022; APIs &#x2022; Node.js &#x2022; Express &#x2022; Sequelize &#x2022; Mongoose &#x2022; GraphQL &#x2022; REST</p>
