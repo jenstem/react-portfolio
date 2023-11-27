9e7a7c28d03673107727c0aa51d34404d9650953
jenstem <124745888+jenstem@users.noreply.github.com>
2023-11-27 16:41:58 -0500

    Add Rhythm Reviews 2.0 to projects.js

--- src/Pages/projects.js
+++ src/Pages/projects.js
@@ -7,8 +7,10 @@ import TechBlog from '../assets/app-images/tech-blog.jpg';
 import RhythmReviews from '../assets/app-images/rhythm-reviews.jpg';
 import ECommerceBackend from '../assets/app-images/ecommerce.jpg';
 import EmployeeTracker from '../assets/app-images/employee-tracker.jpg';
+import RhythmReviews2 from '../assets/app-images/rhythmreview2.jpg';
 
 const projectData = [
+    { title: 'Rhythm Reviews 2.0', description: 'A React review site', imageUrl: RhythmReviews2, url: "https://github.com/group2-project3/rhythm-reviews-2.0", site: "https://rhythm-reviews-v2-a30efa7f58da.herokuapp.com/" },
     { title: 'PWA Text Editor', description: 'Text editor web application', imageUrl: PWA, url: "https://github.com/jenstem/pwa-text-editor", site: "https://hidden-ocean-30849-b1dc3b32b941.herokuapp.com/" },
     { title: 'Social Network API', description: 'A command line invoked network api', imageUrl: SocialNetwork, url: 'https://github.com/jenstem/social-network-api', site: "Not Available" },
     { title: 'Tech Blog', description: 'CMS-style blog site', imageUrl: TechBlog, url: "https://github.com/jenstem/tech-blog", site: "https://fast-brook-86367-d21f795e7f85.herokuapp.com/" },
