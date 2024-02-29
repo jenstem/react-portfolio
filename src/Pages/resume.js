d5b245630d0baca8d43473d8502b918dcc81276c
Jen <124745888+jenstem@users.noreply.github.com>
2024-02-29 15:16:14 -0500

    Update skills content in resume section

--- src/Pages/resume.js
+++ src/Pages/resume.js
@@ -14,7 +14,8 @@ export default function Resume() {
                 </span>{" "}
             </p>
                 <p>HTML &#x2022; CSS &#x2022; JavaScript &#x2022; jQuery &#x2022; React &#x2022; Tailwind &#x2022; Bootstrap &#x2022; Adobe Photoshop</p>
-                <p className='add-space'>MySQL &#x2022; MongoDB &#x2022; APIs &#x2022; Node.js &#x2022; Express &#x2022; Sequelize &#x2022; Mongoose &#x2022; GraphQL &#x2022; REST</p>
+                <p>MySQL &#x2022; MongoDB &#x2022; APIs &#x2022; Node.js &#x2022; Express &#x2022; Sequelize &#x2022; Mongoose &#x2022; GraphQL &#x2022; REST</p>
+                <p className='add-space'>TypeScript &#x2022; Redux &#x2022; Python &#x2022; Flask</p>
             <div className="bar"></div>
         </div>
     );
