e150d8860b644e25c36925a346a35a214ff4e42e
Jen <124745888+jenstem@users.noreply.github.com>
2024-07-02 14:59:16 -0400

    Update skills section

--- src/Pages/resume.js
+++ src/Pages/resume.js
@@ -13,9 +13,20 @@ export default function Resume() {
                     <a href={resumeDoc} target="_blank" rel="noreferrer" className='download-resume'>Click here to download my resume PDF</a>
                 </span>{" "}
             </p>
-                <p>HTML &#x2022; CSS &#x2022; JavaScript &#x2022; jQuery &#x2022; React &#x2022; Tailwind &#x2022; Bootstrap &#x2022; Adobe Photoshop</p>
-                <p>MySQL &#x2022; MongoDB &#x2022; APIs &#x2022; Node.js &#x2022; Express &#x2022; Sequelize &#x2022; Mongoose &#x2022; GraphQL &#x2022; REST</p>
-                <p className='add-space'>TypeScript &#x2022; Redux &#x2022; Python &#x2022; Flask &#x2022; Django</p>
+                <p>Frontend:
+                    HTML &#x2022; CSS &#x2022; JavaScript &#x2022; jQuery &#x2022; React &#x2022; Tailwind &#x2022; Bootstrap &#x2022; Next.js &#x2022;
+                    Redux &#x2022; GraphQL &#x2022; Mongoose &#x2022; TypeScript</p>
+
+                <p>Backend:
+                    Python &#x2022; Django &#x2022; Flask &#x2022; SQLAlchemy &#x2022; Node.js &#x2022; Sequelize &#x2022; Express.js &#x2022; REST API &#x2022; AWS</p>
+
+                <p>Database:
+                    MongoDB &#x2022; MySQL &#x2022; SQL &#x2022; NoSQL &#x2022; PostgreSQL &#x2022; Git</p>
+
+                <p>Tools:
+                    Adobe Photoshop &#x2022; GitHub &#x2022; GitLab &#x2022; VS Code &#x2022; Insomnia &#x2022; Netlify &#x2022;
+                    Heroku &#x2022; Wordpress </p>
+                <p className='add-space'> </p>
             <div className="bar"></div>
         </div>
     );
