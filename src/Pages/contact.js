f59133d8af99b0725f967883ad578c94b4964404
Jen <124745888+jenstem@users.noreply.github.com>
2024-07-25 16:22:47 -0400

    Revise contact page w toast msgs

--- src/Pages/contact.js
+++ src/Pages/contact.js
@@ -1,106 +1,3 @@
-// import React, { useState, useRef } from 'react';
-// import './contact.css';
-// import { validateEmail } from '../utils/helpers';
-// import emailjs from '@emailjs/browser';
-// import { toast } from "sonner"
-
-// export default function Contact() {
-//     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-//     const [errorMessage, setErrorMessage] = useState('');
-//     const { name, email, message } = formState;
-//     const form = useRef();
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         emailjs.sendForm('service_srhstmv', 'template_fcjmcqq', form.current, {
-//                 publicKey: 'sN5KxwZnnbauM-BBg',
-//             })
-//             .then(
-//                 () => {
-//                     toast.success("Message sent successfully"),
-//                     console.log('SUCCESS! Message sent.');
-//                 },
-//                 (error) => {
-//                     toast.error("Message not sent"),
-//                     console.log('FAILED...', error.text);
-//                 },
-//             );
-//         if (!errorMessage) {
-//             console.log('Submit Form', formState);
-//         }
-//         e.target.reset();
-//     };
-
-//     const handleChange = (e) => {
-//         if (e.target.name === "email") {
-//             const isValid = validateEmail(e.target.value);
-//             if (!isValid) {
-//                 setErrorMessage('Your email is invalid');
-//             } else {
-//                 setErrorMessage('');
-//             }
-//         } else {
-//             if (!e.target.value.length) {
-//                 setErrorMessage(`${e.target.name} is required`);
-//             } else {
-//                 setErrorMessage('');
-//             }
-//         }
-//         if (!errorMessage) {
-//             setFormState({ ...formState, [e.target.name]: e.target.value });
-//             console.log('Handle Form', formState);
-//         }
-//     };
-//     return (
-//         <div id="contact">
-//             <div className="contact-form">
-//                 <div className="contact-container-one">
-//                     <h1 id="Contact" className="contact-me">Contact Me</h1>
-//                 </div>
-//                 <div className="contact-container">
-//                     <div className="contact-container-second">
-//                         <div className="form-container">
-//                             <form id="contact-form-id" ref={form} onSubmit={handleSubmit} className="contact-form-class" method="post" action="contact-form-process.php">
-
-//                                 <div className="contact-form-group">
-//                                     <label name="Name" className="contact-label">Your name</label>
-//                                     <div className="contact-input-group">
-//                                         <input type="text" id="Name" name="user_name" className="contact-form-control" defaultValue={name} onBlur={handleChange} placeholder="Enter your name" required />
-//                                     </div>
-//                                 </div>
-
-//                                 <div className="contact-form-group">
-//                                     <label name="Email" className="contact-label">Your email address</label>
-//                                     <div className="contact-input-group">
-//                                         <input type="email" id="Email" name="user_email" defaultValue={email} onBlur={handleChange} className="contact-form-control" placeholder="Enter your email address" required />
-//                                     </div>
-//                                 </div>
-
-//                                 <div className="contact-form-group">
-//                                     <label name="Message" className="contact-label">Your message</label>
-//                                     <div className="contact-input-group">
-//                                         <textarea id="Message" name="message" defaultValue={message} onBlur={handleChange} className="contact-form-control" rows="6" maxLength="3000" placeholder="Enter your message" required></textarea>
-//                                         {errorMessage && (
-//                                             <div>
-//                                                 <p className="error-text">{errorMessage}</p>
-//                                             </div>
-//                                         )}
-//                                     </div>
-//                                 </div>
-//                                 <div className="contact-form-group">
-//                                     <button type="submit" id="contact-button" className="contact-btn contact-btn-primary contact-btn-lg contact-btn-block">Submit</button>
-//                                 </div>
-
-//                             </form>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//             <div className="bar"></div>
-//         </div>
-//     );
-// };
-
-
 import React, { useState, useRef } from 'react';
 import './contact.css';
 import { validateEmail } from '../utils/helpers';
@@ -145,16 +42,8 @@ export default function Contact() {
             <div className="contact-form">
                 <div className="contact-container-one">
                 <h1 id="Contact" className="contact-me">Contact Me</h1>
-/               </div>
-                {/* <div className="contact--info" data-aos="fade-right" data-aos-duration="1000">
-                    <div className="contact--icon">
-                        <span>Tampa, Florida</span>
-                    </div>
+                </div>
 
-                    <div className="contact--icon">
-                        <span>wdiazdev@gmail.com</span>
-                    </div>
-                </div> */}
 
                 <form ref={form} onSubmit={handleSubmit} className="contact-form-group">
                     <div className="contact-form-group">
@@ -168,7 +57,8 @@ export default function Contact() {
                     </div>
 
                     <div className="contact-form-group">
-                        <textarea name="message" required="required" placeholder="" />
+                        <textarea name="message" required="required" placeholder="Message" />
+                        <span>Message</span>
                     </div>
                     <button type="submit" id="contact-button" className="contact-btn contact-btn-primary contact-btn-lg contact-btn-block">
                         {sending ? <SmallLoader /> : "SEND"}
